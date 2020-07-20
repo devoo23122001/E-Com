137e7e312983679a1dedd29e558ef628c5e52711
devoo23122001 <devgoel11@gmail.com>
2020-07-20 13:14:50 +0530

    First Commit

---
+++ shop/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Shop Home")
+
+
+def aboutus(request):
+    return HttpResponse("Shop Home")
+
+
+def contact(request):
+    return HttpResponse("Shop Home")
+
+
+def search(request):
+    return HttpResponse("Shop Home")
+
+
+def productview(request):
+    return HttpResponse("Shop Home")
+
+
+def cart(request):
+    return HttpResponse("Shop Home")
+
+
+def wishlist(request):
+    return HttpResponse("Shop Home")
+
+
+def checkout(request):
+    return HttpResponse("Shop Home")
+
+
+def payment(request):
+    return HttpResponse("Shop Home")
+
+
+def tracker(request):
+    return HttpResponse("Shop Home")
+
+
+def index(request):
+    return HttpResponse("Shop Home")
+
